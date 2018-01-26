7e4b4c4d4175cdb25479404884f79b99466d4bf0
Roberto <roberto.baldi@mail.polimi.it>
2018-01-27 00:13:19 +0100

    Particle System.

---
+++ Assets/Scripts/Piccione.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Piccione : MonoBehaviour {
+    private Rigidbody rb;
+    private ParticleSystem ps;
+
+    public float forceModule;
+
+    private void Start () {
+        rb = GetComponent<Rigidbody>();
+        ps = GetComponent<ParticleSystem>();
+        ps.loop = false;
+
+    }
+
+    private void Update () {
+        if (Input.GetButtonDown("Jump"))
+            rb.AddForce(Vector3.up * forceModule, ForceMode.Impulse);
+	}
+    
+    private void OnTriggerEnter(Collider collision) {
+        if (collision.gameObject.CompareTag("Borders"))
+            ps.Play();
+    }
+}
