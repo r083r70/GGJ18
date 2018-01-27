cf0717f29942c836db642363c5c045533e96b5c1
HP5\Blackfox <oloccin925@hotmail.it>
2018-01-27 20:15:05 +0100

    aggiunta menu principale e carica la scena "particles"

---
+++ Assets/LevelManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelManager : MonoBehaviour {
+
+    public void LoadLevel(string name)
+    {
+        Application.LoadLevel(name);
+    }
+
+    void NextLevel()
+    {
+        Application.LoadLevel(Application.loadedLevel + 1);
+    }
+
+    void QuitGame()
+    {
+        Application.Quit();
+    }
+
+}
