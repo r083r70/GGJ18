4e7742d7924a954117e17a36b9815e0278ac8b57
Surface\marco <marco.forlivesi@outlook.it>
2018-01-27 16:57:16 +0100

    Cleaned code

---
+++ Assets/Scripts/Online/ClientConnection.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.UI;
+
+public class ClientConnection : NetworkBehaviour {
+    public Text errors;
+    private bool connecting;
+
+    void Start()
+    {
+        
+    }
+
+    
+
+    public override void OnStartClient()
+    {
+        base.OnStartClient();
+        if (isLocalPlayer)
+        {
+            errors.text = "0 ASSERT OnConnectedToServer";
+        }
+    }
+
+
+}
