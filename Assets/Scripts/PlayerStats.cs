158b0e2784d6baa0a3e6785388b3fee8871fc96e
HP5\Blackfox <oloccin925@hotmail.it>
2018-01-27 05:33:27 +0100

    tolto un mio script che non centrava più a nulla.

--- Assets/Scripts/PlayerStats.cs
+++ Assets/Scripts/PlayerStats.cs
@@ -7,60 +7,7 @@ public class PlayerStats : MonoBehaviour {
 
     
     public float Health = 100f;
-    public int currentHealth;
-    public Slider sliderHealth;
-    public Color flashColour = new Color(1f, 0f, 0f, 0.1f);
-    public Image imageDamage;
-    bool isDead;
-    bool damaged;
-
-    void Update()
-    {
-        
-        if (damaged)
-        {
-           
-            imageDamage.color = flashColour;
-        }
-        
-        else
-        {
-
-           
-        }
-
-      
-        damaged = false;
-    }
-
-    public void TakeDamage(int amount)
-    {
-        
-        damaged = true;
-
-        
-        currentHealth -= amount;
-
-
-        sliderHealth.value = Health;
-
-        
-        
-
-        
-        if (Health <= 0 && !isDead)
-        {
-            
-            
-        }
-    }
-
-    void Dead()
-    {
-
-    }
-
-
+   
 
 
 }
