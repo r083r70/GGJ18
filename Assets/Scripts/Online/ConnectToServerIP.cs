fe55c729bd6da45893e2be91ea2b886deafbd61f
Surface\marco <marco.forlivesi@outlook.it>
2018-01-27 18:15:43 +0100

    Moved scenes

--- Assets/Scripts/Online/ConnectToServerIP.cs
+++ Assets/Scripts/Online/ConnectToServerIP.cs
@@ -24,8 +24,6 @@ public class ConnectToServerIP : MonoBehaviour {
         }
         ip += ipFields[ipFields.Length - 1].text;
 
-        Debug.Log(ip);
-
         Network.Connect(ip, Manager.serverPort);
     }
 }
