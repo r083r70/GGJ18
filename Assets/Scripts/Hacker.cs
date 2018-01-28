5d5d5a7600e2fa8fc9b593b1353fa19ccecc00a4
Surface\marco <marco.forlivesi@outlook.it>
2018-01-28 11:51:24 +0100

    hacker tocuch

--- Assets/Scripts/Hacker.cs
+++ Assets/Scripts/Hacker.cs
@@ -2,16 +2,21 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Hacker : MonoBehaviour {
+public class Hacker : Photon.MonoBehaviour
+{
     public GameObject obstaclePrefab;
 
 	// Use this for initialization
-	void Start () {
-		
-	}
+	void Update () {
+        if (Input.touchCount > 0 || Input.GetMouseButtonDown(0))
+        {
+            Debug.Log("Hacker obstacle");
+            PhotonNetwork.Instantiate(obstaclePrefab.name, InputToEvent.inputHitPos + new Vector3(0, 5f, 0), Quaternion.identity, 0);
+        }
+    }
 	
 	// Update is called once per frame
-	void OnObstacleTouch () {
-        PhotonNetwork.Instantiate(obstaclePrefab.name, InputToEvent.inputHitPos + new Vector3(0, 5f, 0), Quaternion.identity, 0);
+	public void OnObstacleTouch () {
+        
     }
 }
